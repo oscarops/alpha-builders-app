5f0778cb8169ac37289857da05d2d07d9d556550
oscarops <oscarsebitas2013@gmail.com>
2026-08-18 12:33:17 -0500

    Add files via upload

--- app_streamlit.py
+++ app_streamlit.py
@@ -421,14 +421,34 @@ st.markdown(
 CARGOS_DISPONIBLES = ["Residente", "Asistente", "Maestro Mayor"]
 
 EDIFICIOS_ALPHA = [
-    "Tesla", "Lafuente", "Imagine", "Asimov", "Rubik", "Castle Rock",
-    "Musk", "Wolf", "Dablanc", "Thomas Edison", "Westinghouse", "Smart",
+    "Tesla",
+    "Lafuente",
+    "Imagine",
+    "Asimov",
+    "Rubik",
+    "Castle Rock",
+    "Musk",
+    "Wolf",
+    "Dablanc",
+    "Thomas Edison",
+    "Westinghouse",
+    "Smart",
+    "Magnus",
 ]
 
 NOMBRES_MESES = {
-    1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril",
-    5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
-    9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
+    1: "Enero",
+    2: "Febrero",
+    3: "Marzo",
+    4: "Abril",
+    5: "Mayo",
+    6: "Junio",
+    7: "Julio",
+    8: "Agosto",
+    9: "Septiembre",
+    10: "Octubre",
+    11: "Noviembre",
+    12: "Diciembre",
 }
 
 UNIDADES_RUBRO = {"Enlucidos": "m2", "Fijos": "m2", "Fajas": "m", "Dinteles": "m"}
